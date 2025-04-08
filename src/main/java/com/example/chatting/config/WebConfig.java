cef55723a32992a1c72468912fbc22f1f813e117
kimjuhyeon <kimjh2012kr@gmail.com>
2025-04-08 23:33:54 +0900

    [FIX]: corsMapping 정적리소스 허용

--- src/main/java/com/example/chatting/config/WebConfig.java
+++ src/main/java/com/example/chatting/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
+        registry.addMapping("/**")
                 .allowedOriginPatterns("http://localhost:*",
                         "http://43.200.67.94:30383")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
