ae3d39dc26cea419fec9ec475d02ec8160d9f3cd
김주현 <106982919+kimjuvid@users.noreply.github.com>
2025-04-08 16:42:25 +0900

    Update RedisConfig.java

--- src/main/java/com/example/chatting/config/RedisConfig.java
+++ src/main/java/com/example/chatting/config/RedisConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
@@ -16,7 +17,12 @@ public class RedisConfig {
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory(){
-        return new LettuceConnectionFactory();
+        return new LettuceConnectionFactory(
+            new RedisStandaloneConfiguration(
+            System.getenv("REDIS_HOST"),
+            Integer.parseInt(System.getenv("REDIS_PORT"))
+        )
+        );
     }
 
     @Bean
