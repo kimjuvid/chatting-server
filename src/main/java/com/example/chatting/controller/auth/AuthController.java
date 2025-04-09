27b3581b26ec5e0f956815ab7b3229b87143ba46
김주현 <106982919+kimjuvid@users.noreply.github.com>
2025-04-09 19:09:18 +0900

    Update AuthController.java

--- src/main/java/com/example/chatting/controller/auth/AuthController.java
+++ src/main/java/com/example/chatting/controller/auth/AuthController.java
@@ -7,12 +7,12 @@ import com.example.chatting.model.auth.response.LoginResponse;
 import com.example.chatting.service.auth.AuthService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @Tag(name = "Auth API", description = "v1 Auth API")
 @RestController
@@ -38,5 +38,12 @@ public class AuthController {
     @PostMapping("/login")
     public LoginResponse login(@RequestBody @Valid LoginRequest request){
         return authService.login(request);
+    }
+    
+    // 서버 수평확장 test
+    @GetMapping("/api/ping")
+    public Map<String, String> ping(HttpServletRequest request) {
+        String podName = System.getenv("HOSTNAME"); // Kubernetes에서 pod name으로 자동 주입됨
+        return Map.of("message", "pong", "pod", podName);
     }
 }
